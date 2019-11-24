52225ebead4a151db9dcff445c20b99dc574d9cb
mixheaven <mixheaven@hotmail.com>
2019-11-24 16:44:41 +0100

    fini

--- src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
+++ src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
@@ -1,8 +1,7 @@
 package com.wildcodeschool.wildandwizard.repository;
 
+
 import com.wildcodeschool.wildandwizard.entity.School;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -91,6 +90,36 @@ public class SchoolRepository {
             return schools;
         } catch (
                 SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public School save(String name,Long capacity, String country) {
+
+        try {
+            Connection connection = DriverManager.getConnection(
+                    DB_URL, DB_USER, DB_PASSWORD
+            );
+            PreparedStatement statement = connection.prepareStatement(
+                    "INSERT INTO school (name, capacity, country) VALUES (?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS
+            );
+            statement.setString(1, name);
+            statement.setLong(2, capacity);
+            statement.setString(3, country);
+            if (statement.executeUpdate() != 1) {
+                throw new SQLException("failed to insert data");
+            }
+
+            ResultSet generatedKeys = statement.getGeneratedKeys();
+
+            if (generatedKeys.next()) {
+                Long id = generatedKeys.getLong(1);
+                return new School(id, name, capacity, country);
+            } else {
+                throw new SQLException("failed to get inserted id");
+            }
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return null;
